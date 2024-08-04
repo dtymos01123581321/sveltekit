5cc8a98b001175eabb4b4cb7a16df0ac7223893d
Dmytro Tymoshyk <dtymoshyk@360insights.com>
2024-08-04 21:41:44 +0300

    create project

--- svelte.config.js
+++
@@ -1,9 +0,0 @@
-import adapter from '@sveltejs/adapter-cloudflare';
-import preprocess from 'svelte-preprocess';
-
-export default {
-    kit: {
-        adapter: adapter(),
-    },
-    preprocess: preprocess()
-};
